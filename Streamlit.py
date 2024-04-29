9fc74517f41889cc96bc3565340c7a3b2b4a509a
Trixiemoek <145706145+Trixiemoek@users.noreply.github.com>
2024-04-29 22:30:04 +0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,16 +1,1 @@
-import streamlit as st
-from rasa.core.agent import Agent
-rasa_agent = Agent.load(r'C:\Users\HomePC\Documents\Mental Health\models\20240425-203440-charcoal-mayonnaise.tar.gz')
-# Streamlit title and header
-st.title("Msiri")
-st.header("Msiri keeps your Secrets")
 
-# Text input for user input
-user_input = st.text_input("You:", "")
-
-# Button to send user input
-if st.button("Send"):
-    # Call Rasa to get bot response
-    bot_response = rasa_agent.handle_text(user_input)
-    # Display bot response
-    st.text("Bot:", bot_response[0]['text'])
